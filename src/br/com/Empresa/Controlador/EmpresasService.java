1204c58a9a50110af601fba934ebf2b394e667a1
YgorRegato <58819037+YgorRegato@users.noreply.github.com>
2020-01-02 20:43:07 -0300

    Adicionando as bibliotecas 'xmlpull' e xstream'

--- src/br/com/Empresa/Controlador/EmpresasService.java
+++ src/br/com/Empresa/Controlador/EmpresasService.java
@@ -10,30 +10,38 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.com.Modelo.Banco;
 import br.com.Modelo.Empresa;
 
-
 @WebServlet("/empresas")
 public class EmpresasService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-	List<Empresa> empresas= new Banco().getEmpresas();
-	
-	Gson gson= new Gson();
-	
-	String json= gson.toJson(empresas);
-	
-	response.setContentType("application/json");
-	response.getWriter().print(json);
-	
-	
-	
-	
-	
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		List<Empresa> empresas = new Banco().getEmpresas();
+
+		XStream xstream = new XStream();
+		xstream.alias("empresa", Empresa.class);
+		String xml = xstream.toXML(empresas);
+
+		response.setContentType("application/xml");
+		response.getWriter().print(xml);
+		
+		
+		
+		
+
+//	Gson gson= new Gson();
+//	
+//	String json= gson.toJson(empresas);
+//	
+//	response.setContentType("application/json");
+//	response.getWriter().print(json);
+
 	}
 
 }
